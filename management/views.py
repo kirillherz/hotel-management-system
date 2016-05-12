256305eb3d6b7ff00896970e97fb4dfe428bce1f
kirillherz <kirill.herz@gmail.com>
2016-05-12 13:30:10 +0300

    Исправлен баг изменением sql запроса

--- management/views.py
+++ management/views.py
@@ -20,26 +20,31 @@ class SelectDateForm(forms.Form):
 def listRooms(request):
     sql = """
     SELECT 
-      management_room.number, management_room.id,
-      management_tariff.name as tarrif_name, management_tariff.units as units,
-      management_valuta.name as valuta_name, management_valuta.abbreviation as valuta_abbreviation
+      management_room.number, management_room.id
     from 
-      management_room, 
-      management_tariff,
-      management_valuta
+      management_room
     where 
-      management_room.id IS NOT (SELECT management_record.room_id from management_record where
-      management_record.date_in > %s and management_record.date_out < %s )
-    and 
-      management_tariff.id = management_room.tariff_id
-    and 
-      management_valuta.id = management_tariff.valuta_id
+      management_room.id =
+      (
+	   SELECT management_record.room_id 
+	   from management_record 
+          where 
+	      (management_record.date_in < %s 
+		   and management_record.date_in < %s
+		   and management_record.date_out < %s
+		   and management_record.date_out < %s)
+	   or 
+	      (management_record.date_in > %s 
+		   and management_record.date_in > %s
+		   and management_record.date_out > %s
+		   and management_record.date_out > %s)
+	   )
              """
     form = SelectDateForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():   
         date_in = form.cleaned_data['date_in']
         date_out = form.cleaned_data['date_out']
-        rooms = Room.objects.raw(sql,[str(date_in), str(date_out)])
+        rooms = Room.objects.raw(sql,[str(date_in), str(date_out)]*4)
     else:
         rooms = None
     return render(request, 'list_rooms.html',{
